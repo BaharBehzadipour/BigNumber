d60542cfd62cbebe0042523111de01d4b28a2687
farhangd <44467574+farhangd@users.noreply.github.com>
2020-05-11 11:31:17 +0430

    add class BigNumber add data members

---
+++ BigNumber.h
@@ -0,0 +1,18 @@
+#ifndef TA14_BIGNUMBER_H
+#define TA14_BIGNUMBER_H
+
+#include <cstdint>
+
+class BigNumber {
+private:
+    bool sign;
+    int8_t * numArray;
+    unsigned numOfDigits;
+
+public:
+
+
+};
+
+
+#endif //TA14_BIGNUMBER_H
---
+++ cmake-build-debug/CMakeFiles/TA14.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TA14.dir/BigNumber.cpp.o"
+  "CMakeFiles/TA14.dir/main.cpp.o"
+  "TA14.exe"
+  "TA14.pdb"
+  "libTA14.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TA14.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
